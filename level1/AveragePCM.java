a98264728a6bda34504fe272e290a0ae06c3b4a5
Anurag Pandey <anuragpandey@Anurags-MacBook-Air-118.local>
2026-02-16 13:22:27 +0530

    week 1 and 3

---
+++ level1/AveragePCM.java
@@ -0,0 +1,16 @@
+class AveragePCM {
+
+    public static void main(String[] args) {
+
+        String studentName = "Sam";
+
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println(studentName + "'s average mark in PCM is " + average);
+
+    }
+}

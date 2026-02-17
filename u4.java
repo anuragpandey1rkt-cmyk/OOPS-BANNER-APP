bd0a60712c9ad964d7a66a5ac9062f1da8eac252
Anurag Pandey <anuragpandey@Anurags-MacBook-Air-118.local>
2026-02-17 13:39:52 +0530

    Uc4

---
+++ u4.java
@@ -0,0 +1,16 @@
+class u4{
+    public static void main(String[] args) {
+        String[] banners = new String[7];
+        banners[0] = String.join("", "   ***  ", "    ***  ", " ******   ", "    *****   ");
+        banners[1] = String.join("", " **  ** ", "  **  ** ", " **    ** ", "  **        ");
+        banners[2] = String.join("", "**    **", " **    **", " **     **", " **         ");
+        banners[3] = String.join("", "**    **", " **    **", " **    ** ", "   *****    ");
+        banners[4] = String.join("", "**    **", " **    **", " ******   ", "         ** ");
+        banners[5] = String.join("", " **  ** ", "  **  ** ", " **       ", "  **     ** ");
+        banners[6] = String.join("", "  ***   ", "   ***   ", " **       ", "    *****   ");
+
+        for (int i = 0; i < banners.length; i++) {
+            System.out.println(banners[i]);
+        }
+    }
+}
